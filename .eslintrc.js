317e5a99a9b10a3a9d30c20fddded4c524a390ff
Vladislav <vlad.devidson.harle@gmail.com>
2023-07-07 13:43:41 +0300

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,5 +12,12 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
+    'no-unused-vars': 0,
+    'no-console': 0,
+    'no-alert': 0,
+    'no-unused-expressions': 0,
+    'import/no-extraneous-dependencies': 0,
+    'import/no-named-default': 0,
+    'no-undef': 0,
   },
 };
